cf504775fee1332cc9279065e107d2cb61b74708
Romu Tiwari <tromu98@gmail.com>
2024-04-05 01:43:36 +0530

    Node App Deploy

--- mongoC.js
+++ mongoC.js
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 
 const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
-const connectionString = `mongodb+srv://110298romu:${password}@github-actions-test-db.z4lcjaj.mongodb.net/?retryWrites=true&w=majority&appName=github-actions-test-db`; // clustore url
+const connectionString = `mongodb+srv://110298romu:${password}@github-actions-test-db.z4lcjaj.mongodb.net/?retryWrites=true&w=majority&appName=github-actions-test-db`; // cluster url
 const client = new MongoClient(connectionString);
 let conn;
 try {
